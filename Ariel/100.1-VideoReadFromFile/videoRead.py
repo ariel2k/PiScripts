d61815977234147e646468862489c8d2572484bc
Ariel Molina <arielhmolina@gmail.com>
2018-07-07 19:01:08 -0300

    100.1 VideoTracking

---
+++ Ariel/100.1-VideoReadFromFile/videoRead.py
@@ -0,0 +1,25 @@
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help = "path to the video")
+args = vars(ap.parse_args())
+
+if args["video"] is None:
+	cap = cv2.VideoCapture()
+else:
+	cap = cv2.VideoCapture(args["video"])
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

553933841de16b24698977c20f97f3f2c020d54d
gabybonaventura <gabybonaventura@hotmail.com>
2018-07-08 18:38:00 -0300

    Adaptacion de codigo y pruebas

--- Gabriel/01-PiCameraStreaming/Streaming.py
+++ Gabriel/01-PiCameraStreaming/Streaming.py
@@ -17,15 +17,43 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
-	image = frame.array
- 
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+	#image = frame.array
+
+
+
+##############################
+    # Take each frame
+    _, image = frame.array
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([10,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(image,image, mask= mask)
+
+    cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
+    cv2.imshow('frame',image)
+    cv2.resizeWindow('frame',400,250)
+
+    cv2.namedWindow('mask',cv2.WINDOW_NORMAL)
+    cv2.imshow('mask',mask)
+    cv2.resizeWindow('mask',400,250)
+
+    cv2.namedWindow('res',cv2.WINDOW_NORMAL)
+    cv2.imshow('res',res)
+    cv2.resizeWindow('res',400,250)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+##############################
+cv2.destroyAllWindows()
+
+

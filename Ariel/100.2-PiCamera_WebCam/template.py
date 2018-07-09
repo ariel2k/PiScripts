dac2eaa45952907b754d298631ea22740a543457
Ariel Molina <arielhmolina@gmail.com>
2018-07-09 16:38:38 -0300

    Template WebCam Ok

--- Ariel/100.2-PiCamera_WebCam/template.py
+++ Ariel/100.2-PiCamera_WebCam/template.py
@@ -16,81 +16,27 @@ def getArgs():
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video", 
         help = "path to the video (optional) default WebCam")
-    ap.add_argument("-pi", "--picamera", 
-        help = "if use PiCamera (optional) default 0")
     return vars(ap.parse_args())
 
-################ INIT ########################
 def getVideoSource(args):
-    if args["picamera"] is not None:
-        return initPiCamera()
     if args["video"] is not None:
-        return initVideoFile(args["video"])
+        return cv2.VideoCapture(args["video"])
     else:
-        return initWebCam()
+        return cv2.VideoCapture(0)
 
-def initPiCamera():
-    from picamera import PiCamera
-    import time
-    camera = PiCamera()
-    camera.framerate = 32
-    return camera
-
-def initVideoFile(path):
-    return cv2.VideoCapture(path)
-
-def initWebCam():
-    return cv2.VideoCapture(0)
-
-################ FRAME ########################
-def getFrameNoPi(args, videoSource):
-    if args["video"] is not None:
-        _,frame = getFrameVideoFile(videoSource)
-    else:
-        _,frame = getFrameWebCam(videoSource)
+def getFrame(videoSource):
+    _,frame = videoSource.read()
     return frame
-
-def getFrameVideoFile(videoSource):
-    return videoSource.read()
-
-def getFrameWebCam(videoSource):
-    return videoSource.read()
-
-################ ISPI ########################
-def isPiCamera(args):
-    return args["picamera"] is not None
-
-################  PI  ########################
-def yesPiCamera(videoSource):
-    from picamera.array import PiRGBArray
-    rawCapture = PiRGBArray(videoSource, size=(640, 480))
-    for frame in videoSource.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
-        action(frame)
-        # clear the stream in preparation for the next frame
-        rawCapture.truncate(0)
-        # Exit
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
-
-################ NOPI ########################
-def noPiCamera(arg,videoSource):
-    while(1):
-        # ReadFrame
-        frame = getFrameNoPi(args, videoSource)
-        action(frame)
-
-        # Exit
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
 
 ################ MAIN ########################
 args = getArgs()
 vs = getVideoSource(args)
-if isPiCamera(args):
-    yesPiCamera(vs)
-else:
-    noPiCamera(args,vs)
+while(1):
+    # ReadFrame
+    frame = getFrame(vs)
+    action(frame)
+    # Exit
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
 cv2.destroyAllWindows()
